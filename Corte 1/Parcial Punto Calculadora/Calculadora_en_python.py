97ca6d96d6d0a8caa0b3506833a3cdda8bbfa31a
e-upho <juan.sramirez2906@gmail.com>
2025-05-07 01:51:44 -0500

    .

---
+++ Corte 1/Parcial Punto Calculadora/Calculadora_en_python.py
@@ -0,0 +1,30 @@
+class Calculadora:
+
+    def sumar(self, a, b):
+        return a + b
+
+    def restar(self, a, b):
+        return a - b
+
+    def multiplicar(self, a, b):
+        return a * b
+
+    def dividir(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return "Error: División por cero no permitida."
+
+    def modulo(self, a, b):
+        try:
+            return a % b
+        except ZeroDivisionError:
+            return "Error: División por cero no permitida."
+
+if __name__ == "__main__":
+    mi_calculadora = Calculadora()
+    print(mi_calculadora.sumar(5, 7))
+    print(mi_calculadora.restar(45, 11))
+    print(mi_calculadora.multiplicar(3, 2))
+    print(mi_calculadora.dividir(10, 0))   
+    print(mi_calculadora.modulo(10, 0))  
